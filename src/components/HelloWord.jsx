6bdf6f286c0bd9ae543b0883cfedcf6863f25ece
Kamerr Ezz <proyects@kamerrezz.com>
2020-09-29 14:01:37 -0500

    entorno de trabajo profesional

---
+++ src/components/HelloWord.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import '../assets/style/App.scss'
+
+export class HelloWord extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hola Mundo</h1>   
+            </div>
+        )
+    }
+}
+
+export default HelloWord
